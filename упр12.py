d5a1c6a75d3800116e492fb5115fe509e2591e1b
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2021-09-20 19:10:30 +0300

    Add files via upload

---
+++ упр12.py
@@ -0,0 +1,13 @@
+import turtle
+
+print('Введите число')
+turtle.shape('turtle')
+turtle.left(90)
+a=int(input())
+n=0
+while n<a:
+    turtle.circle(45, 180)
+    turtle.circle(10, 180)
+    n+=1    
+    
+
