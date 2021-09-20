c3af25d76656a07579c17cfe4b9dfe78c9469b50
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2021-09-20 15:03:13 +0300

    Add files via upload

---
+++ упр5.py
@@ -0,0 +1,85 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.color('pink')
+for i in range(4):
+    turtle.forward(10)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-5, -5)
+turtle.pendown()
+
+turtle.color('violet')
+for i in range(4):
+    turtle.forward(20)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-10, -10)
+turtle.pendown()
+
+turtle.color('purple')
+for i in range(4):
+    turtle.forward(30)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-15, -15)
+turtle.pendown()
+
+turtle.color('dark blue')
+for i in range(4):
+    turtle.forward(40)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-20, -20)
+turtle.pendown()
+
+turtle.color('blue')
+for i in range(4):
+    turtle.forward(50)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-25, -25)
+turtle.pendown()
+
+turtle.color('light blue')
+for i in range(4):
+    turtle.forward(60)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-30, -30)
+turtle.pendown()
+
+turtle.color('green')
+for i in range(4):
+    turtle.forward(70)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-35, -35)
+turtle.pendown()
+
+turtle.color('yellow')
+for i in range(4):
+    turtle.forward(80)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-40, -40)
+turtle.pendown()
+
+turtle.color('orange')
+for i in range(4):
+    turtle.forward(90)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-45, -45)
+turtle.pendown()
+
+turtle.color('red')
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+turtle.penup()
+turtle.goto(-50, -50)
+turtle.pendown()
+
+
+
