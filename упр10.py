f51ef92c081184e3122cd4d0741ccc9a87fe3fef
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2021-09-20 17:55:44 +0300

    Add files via upload

---
+++ упр10.py
@@ -0,0 +1,14 @@
+import turtle
+import math
+
+turtle.shape('turtle')
+for i in range (3):
+    n=0
+    while n<2:
+        turtle.circle(45)
+        turtle.left(180)
+        n+=1
+    turtle.left(60)
+    
+    
+
