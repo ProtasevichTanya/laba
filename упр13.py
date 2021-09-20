a0d3ac34ba331e6317cb0f37607d8de87de64aa9
DashaKrivosheina <91007535+DashaKrivosheina@users.noreply.github.com>
2021-09-20 19:31:04 +0300

    Add files via upload

---
+++ упр13.py
@@ -0,0 +1,47 @@
+import turtle
+
+turtle.shape('turtle')
+turtle.pencolor('black')
+turtle.fillcolor('yellow')
+turtle.begin_fill()
+turtle.circle(80)
+turtle.end_fill()
+
+turtle.up()
+turtle.goto(-30, 100)
+turtle.down()
+turtle.begin_fill()
+turtle.pencolor('black')
+turtle.fillcolor('blue')
+turtle.circle(13)
+turtle.end_fill()
+
+turtle.up()
+turtle.goto(30, 100)
+turtle.down()
+turtle.begin_fill()
+turtle.pencolor('black')
+turtle.fillcolor('blue')
+turtle.circle(13)
+turtle.end_fill()
+
+turtle.up()
+turtle.goto(0, 70)
+turtle.down()
+turtle.begin_fill()
+turtle.pencolor('black')
+turtle.fillcolor('brown')
+turtle.width(9)
+turtle.goto(0, 90)
+
+turtle.up()
+turtle.goto(-30, 50)
+turtle.right(90)
+turtle.width(6)
+turtle.down()
+turtle.color('red')
+turtle.circle(30, 180)
+
+
+    
+
