c5684e8697242c2c36f0069c7d8469f39ea8d0ab
Protasevich Tanya <protasevich.ts@phystech.edu>
2021-10-12 17:40:22 +0300

    improve cats

--- laba3/3_8cats.py
+++ laba3/3_8cats.py
@@ -4,7 +4,7 @@ from pygame.draw import *
 pygame.init()
 
 #функция рисования кота
-def cat(x1, y1, x2, y2, colorc, colory):
+def cat(x1, y1, x2, y2, colorc, colory, angle):
     '''
     Функция рисует кота на экране
     Ориентация кота зависит от того, как расположена точка (x2, y2) относительно (x1,y1).
@@ -13,126 +13,136 @@ def cat(x1, y1, x2, y2, colorc, colory):
     x2 и y2 - координаты правого нижнего угла (если смотрит влево), левого - если вправо
     colorc - цвет кота
     colory - цвет глаз
+    angle - угол поворота кота относительно верхней левой точки экрана
 
     ВАЖНО!!!
     координата y2 обязательно должна быть больше y1
     '''
+    # создание поверхности котика
+    cat_surface = pygame.Surface((700, 700), pygame.SRCALPHA, 32)
+  
     a = y2 - y1
     b = x2 - x1
     if b >= 0:
         # нога за головой
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 20, y1 + a / 2, b / 8, a / 2))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 20, y1 + a / 2, b / 8, a / 2), 2)
         # хвост
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b * 7 / 8, y1 + a / 2, b / 3, a / 3))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b * 7 / 8, y1 + a / 2, b / 3, a / 3), 2)
         # тело
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 8, y1 + 1 / 5 * a, 5 * b / 6, a * 4 / 5))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 8, y1 + 1 / 5 * a, 5 * b / 6, a * 4 / 5), 2)
         # голова 
-        circle(sc, colorc,
+        circle(cat_surface, colorc,
            (x1 + b / 6, y1 + a / 2), a / 3) 
-        circle(sc, WHITE,
+        circle(cat_surface, WHITE,
            (x1 + b / 6, y1 + a / 2), a / 3, 2)
         # передняя нога вторая
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 6, y1 + a * 6 / 7, b / 5, a / 5))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 6, y1 + a * 6 / 7, b / 5, a / 5), 2)
         # круг задней ноги
-        circle(sc, colorc,
+        circle(cat_surface, colorc,
            (x1 + 5 * b / 6, y1 + 4 / 5 * a), a / 4)  
-        circle(sc, WHITE,
+        circle(cat_surface, WHITE,
            (x1 + 5 * b / 6, y1 + 4 / 5 * a), a / 4, 2)
         # стопа задней ноги
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b * 7 / 8, y1 + a * 7 / 8, b / 8, a / 3))
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b * 7 / 8, y1 + a * 7 / 8, b / 8, a / 3), 2)  
         # глаза
         for i in (-1, 1):
-            circle(sc, colory,
+            circle(cat_surface, colory,
                (x1 + b / 6 + i * b / 15, y1 + a / 2 - a / 10), a / 14)  
-            circle(sc, BLACK,
+            circle(cat_surface, BLACK,
                (x1 + b / 6 + i * b / 15, y1 + a / 2 - a / 10), a / 14, 1)
-            ellipse(sc, BLACK,
+            ellipse(cat_surface, BLACK,
                 (x1 + b / 6 + i * b / 15 + b / 100, y1 + a / 2 - a / 10 - a / 20, b / 100, a / 10))
     else:
         # нога за головой
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 20, y1 + a / 2, b / 8, a / 2))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 20 + b / 8, y1 + a / 2, - b / 8 + 1, a / 2), 2)
         # хвост
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b * 7 / 8, y1 + a / 2, b / 3, a / 3)) 
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b * 7 / 8 + b / 3, y1 + a / 2, - b / 3 + 1, a / 3), 2)
         # тело
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 8, y1 + 1 / 5 * a, 5 * b / 6, a * 4 / 5))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 8 + 5 * b / 6, y1 + 1 / 5 * a, - 5 * b / 6, a * 4 / 5), 2)
         # голова
-        circle(sc, colorc,
+        circle(cat_surface, colorc,
            (x1 + b / 6, y1 + a / 2), a / 3)  
-        circle(sc, WHITE,
+        circle(cat_surface, WHITE,
            (x1 + b / 6, y1 + a / 2), a / 3, 2)
         # передняя нога вторая
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b / 6, y1 + a * 6 / 7, b / 5, a / 5))  
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b / 6 + b / 5, y1 + a * 6 / 7, - b / 5 + 1, a / 5), 2)
         # круг задней ноги
-        circle(sc, colorc,
+        circle(cat_surface, colorc,
            (x1 + 5 * b / 6, y1 + 4 / 5 * a), a / 4)  
-        circle(sc, WHITE,
+        circle(cat_surface, WHITE,
            (x1 + 5 * b / 6, y1 + 4 / 5 * a), a / 4, 2)
         # стопа задней ноги
-        ellipse(sc, colorc,
+        ellipse(cat_surface, colorc,
             (x1 + b * 7 / 8, y1 + a * 7 / 8, b / 8, a / 3)) 
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
             (x1 + b * 7 / 8 + b / 8, y1 + a * 7 / 8, - b / 8 + 1, a / 3), 2)
         # глаза
         for i in (-1, 1):
-            circle(sc, colory,
+            circle(cat_surface, colory,
                (x1 + b / 6 + i * b / 15, y1 + a / 2 - a / 10), a / 14)  
-            circle(sc, BLACK,
+            circle(cat_surface, BLACK,
                (x1 + b / 6 + i * b / 15, y1 + a / 2 - a / 10), a / 14, 1)
-            ellipse(sc, BLACK,
+            ellipse(cat_surface, BLACK,
                 (x1 + b / 6 + i * b / 15 + b / 100, y1 + a / 2 - a / 10 - a / 20, b / 100, a / 10))
             
     # рисуем уши 
-    polygon(sc, WHITE,
+    polygon(cat_surface, WHITE,
                         [(x1 + (0.316 * b) * 2 / 3, y1 + 0.19 * a), (x1 + (0.44 * b) * 2 / 3, y1 + 0.314 * a),
                          (x1 + (0.45 * b) * 2 / 3, y1 + 0.1 * a)], 4)  
-    polygon(sc, colorc,
+    polygon(cat_surface, colorc,
                         [(x1 + (0.316 * b) * 2 / 3, y1 + 0.19 * a), (x1 + (0.44 * b) * 2 / 3, y1 + 0.314 * a),
                          (x1 + (0.45 * b) * 2 / 3, y1 + 0.1 * a)])
-    polygon(sc, WHITE,
+    polygon(cat_surface, WHITE,
                         [(x1 + b / 10, y1 + 0.19 * a), (x1 - (0.12 * b) * 2 / 3 + b / 10, y1 + 0.314 * a),
                          (x1 - (0.17 * b) * 2 / 3 + b / 10, y1 + 0.1 * a)], 5)
-    polygon(sc, colorc,
+    polygon(cat_surface, colorc,
                         [(x1 + b / 10, y1 + 0.19 * a), (x1 - (0.12 * b) * 2 / 3 + b / 10, y1 + 0.314 * a),
                          (x1 - (0.17 * b) * 2 / 3 + b / 10, y1 + 0.1 * a)])
     # рисуем нос
-    polygon(sc, WHITE,
+    polygon(cat_surface, WHITE,
                         [(x1 + b / 6 + b / 50, y1 + a / 2 - a / 50), (x1 + b / 6 - b / 50, y1 + a / 2 - a / 50),
                          (x1 + b / 6, y1 + a / 2)], 2)
     # рисуем усы
     for i in range(5):
-        line(sc, WHITE, [x1 + b / 6 + b / 30, y1 + a / 2 + a / 20 + a / 50 * i],
+        line(cat_surface, WHITE, [x1 + b / 6 + b / 30, y1 + a / 2 + a / 20 + a / 50 * i],
                         [x1 + b / 6 + b / 4, y1 + a / 2 - a / 30 + a / 20 + a / 35 * i], 1)  
-        line(sc, WHITE, (x1 + b / 6 - b / 30, y1 + a / 2 + a / 20 + a / 50 * i),
+        line(cat_surface, WHITE, (x1 + b / 6 - b / 30, y1 + a / 2 + a / 20 + a / 50 * i),
                         [x1 + b / 6 - b / 4, y1 + a / 2 - a / 30 + a / 20 + a / 35 * i], 1)
-        ellipse(sc, WHITE,
+        ellipse(cat_surface, WHITE,
                 (x1 + b / 6, y1 + a / 2, 2, a / 8), 2)
+
+    # поворот кота
+    catsur=pygame.transform.rotate(cat_surface, angle)
+    sc.blit(catsur, (0, 0))
+
+    
 
 # функция рисования клубка
 def klu(x1, y1, colk, r, napr):
@@ -192,16 +202,16 @@ ORANGE = (254,102,13)
 FPS = 30
 sc = pygame.display.set_mode((700, 700)) # размеры окна
 
-#рисуем стену и пол
+# рисуем стену и пол
 polygon(sc, STENA, [(0, 0), (0, 300), (700, 300), (700, 0)])
 polygon(sc, POL, [(0, 300), (0, 700), (700, 700), (700, 300)])
 
-# задаем положение, размер и цвет котов
-cat(300, 300, 500, 400, GRAY, BLUE)
-cat(500, 300, 600, 350, ORANGE, GREEN)
-cat(100, 500, 300, 600, GRAY, BLUE)
-cat(250, 250, 50, 350, ORANGE, GREEN)
-cat(500, 500, 400, 550, ORANGE, GREEN)
+# задаем положение, размер, цвет и угол поворота котов
+cat(300, 300, 500, 400, GRAY, BLUE, 0)
+cat(500, 300, 600, 350, ORANGE, GREEN, 0)
+cat(100, 500, 300, 600, GRAY, BLUE, 0)
+cat(250, 250, 50, 350, ORANGE, GREEN, 0)
+cat(500, 500, 400, 550, ORANGE, GREEN, 0)
 
 # рисуем клубки
 klu(400, 400, PINK, 30, -1)
